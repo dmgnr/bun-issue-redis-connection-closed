13c1c8f426e0b23a16c303a4f65f97875d666bde
dmgnr <me@dgnr.us>
2026-09-21 17:58:12 +0000

    add possible workarounds

--- worker.ts
+++ worker.ts
@@ -1,10 +1,20 @@
 import { redis, sleep } from "bun";
+// WORKAROUND B, essentially infinite retry count
+// const redis = new RedisClient(undefined, {
+//   maxRetries: 4294967295,
+// });
 
 for (;;) {
   try {
     console.log(" Working", await redis.incr("test"));
   } catch (e) {
-    console.log(" Failing", (e as any).message ?? e);
+    const msg = (e as any).message as string;
+    console.log(" Failing", msg ?? e);
+    // -- WORKAROUND A, resetting attempts count
+    // if (msg.includes("Connection has failed")) {
+    //   console.log(" Attempting manual reconnection");
+    //   redis.connect();
+    // }
   }
   await sleep(1000);
 }
